29cae999fd377e30bebb6ca4bc445577f9bd1f62
Mruczacy <mruczacy-kontakt@wp.pl>
2021-03-18 20:20:48 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const configcontent = require("./config.json")
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log("Imma ready");
+    let user = client.users.cache.get("502146791597277194");
+    user.send("Imma ready bro")
+});
+
+client.on("message", message => {
+    const args = message.content.trim().split(' ');
+    if (client.user.id != message.author.id) {
+        for (let i = 0; i < args.length; i++) {
+            if (configcontent.niggalist.includes(args[i])) {
+                let channel = client.channels.cache.get("715643100693004338");
+                channel.send("User " + message.author.tag + " has used bad word\nLink is here: https://discord.com/channels/" + message.guild.id + "/" + message.channel.id + "/" + message.id);
+                return;
+            }
+        }
+    }
+});
+
+client.login(configcontent.token);
